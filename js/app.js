8ce570cc2cb8b7082f266e399a96043eb17f1a16
João Marcos <joaomarcossm2013@hotmail.com>
2020-08-26 19:54:58 -0300

    V1.02

--- js/app.js
+++ js/app.js
@@ -15,8 +15,6 @@ function appendLocation(location, verb) {
     let newLocationMap = document.getElementById('locationOn');
     newLocationMap.src = 'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d29329.604239799035!2d' + location.coords.longitude + '!3d' + location.coords.latitude + '!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1598361176620!5m2!1spt-BR!2sbr';
 
-    target.appendChild(newLocation);
-    target.appendChild(newLocationMap);
 }
 
 if ('geolocation' in navigator) {
